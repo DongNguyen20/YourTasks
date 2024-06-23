d00a55b4930f673a0fd2c0936b94f568a9a4a0d9
DongNguyen20 <dongtruelove@gmail.com>
2024-06-23 09:47:59 +0700

    init project

---
+++ yourtasks-api/src/main/java/com/kopw/yourtasks/YourtasksApiApplication.java
@@ -0,0 +1,13 @@
+package com.kopw.yourtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourtasksApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourtasksApiApplication.class, args);
+    }
+
+}
